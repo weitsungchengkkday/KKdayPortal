13fdf49ce18851b3123a221a78248d7cf2af8c07
wei-tsung-cheng <sanctuary85c@gmail.com>
2019-11-21 10:20:04 +0800

    [feat] add KKdayPortal and podfile

---
+++ KKdayPortal/KKdayPortal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KKdayPortal
+//
+//  Created by WEI-TSUNG CHENG on 2019/11/21.
+//  Copyright © 2019 WEI-TSUNG CHENG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
