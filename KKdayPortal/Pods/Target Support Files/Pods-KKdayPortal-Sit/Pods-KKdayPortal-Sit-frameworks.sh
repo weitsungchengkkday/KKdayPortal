4c49063416dd563dd0b340480f846e2ec85fbc65
william.cheng <william.cheng@kkday.com>
2020-12-10 16:26:18 +0800

    [pod] install TwilioVoice

--- KKdayPortal/Pods/Target Support Files/Pods-KKdayPortal-Sit/Pods-KKdayPortal-Sit-frameworks.sh
+++ KKdayPortal/Pods/Target Support Files/Pods-KKdayPortal-Sit/Pods-KKdayPortal-Sit-frameworks.sh
@@ -178,11 +178,13 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/HTMLString/HTMLString.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SwiftSoup/SwiftSoup.framework"
+  install_framework "${PODS_ROOT}/TwilioVoice/Build/iOS/TwilioVoice.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/HTMLString/HTMLString.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SwiftSoup/SwiftSoup.framework"
+  install_framework "${PODS_ROOT}/TwilioVoice/Build/iOS/TwilioVoice.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
